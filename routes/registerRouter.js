4ed89a5842b1ae088ed7d83cc95392a8a478650b
unknown <donnymrshd.94@gmail.com>
2021-08-12 01:57:50 +0700

    updates server

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { v4: uuidv4 } = require('uuid');
 const moment = require('moment');
 const fs = require('fs');
